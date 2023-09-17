11f997be95686f940a99b8f3b4e64e91f44a6f81
MalfinK <muhammadalfin1235678@gmail.com>
2023-09-17 22:18:44 +0700

    udah bener tapi masih perlu di rev bagian index dari detail prediksinya

--- model.py
+++ model.py
@@ -1,9 +1,11 @@
 # %%
 import pandas as pd
+import sys
 
 from sklearn.naive_bayes import ComplementNB
+from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, confusion_matrix
 
 
 # %%
@@ -11,6 +13,13 @@ from sklearn.metrics import accuracy_score
 datas3 = pd.read_csv("iris.csv");
 x = datas3.iloc[:,:4]
 y = datas3.iloc[:,4]
+
+# tambahkan nama kolom di datas3 dan edit ke csv
+datas3.columns = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'class']
+# datas3.to_csv('iris2.csv', index=True)
+# datas3.index = datas3.index + 1
+datas3.to_csv('iris2.csv', index_label='no')
+# sys.exit()
 
 # printing data untuk x dan y
 print(x)
@@ -33,15 +42,24 @@ classifier = ComplementNB()
 classifier.fit(x_train, y_train) # melatih model dengan 60 data latih
 
 y_pred = classifier.predict(x_test) # melakukan prediksi terhadap data uji
-test = pd.DataFrame(y_pred)
-print(test)
+# print(y_pred)
+
+
+test_y_pred = pd.DataFrame(y_pred)
+test_y_pred.columns = ['class']
+print(test_y_pred)
 
 print()
 
-print(pd.DataFrame(x_test))
+test_x_test = pd.DataFrame(x_test)
+# buat varibel test di print tanpa index
+test_x_test = test_x_test.reset_index(drop=True)
+print(test_x_test)
+# sys.exit()
 
-excel_data = pd.concat([pd.DataFrame(x_test), test], axis=1)
-# print(excel_data)
+# concat x_test dan y_pred
+excel_data = pd.concat([test_x_test, test_y_pred], axis=1)
+print(excel_data)
 
 # excel_data = {
 #     'test' : x_test, 'class': y_pred
@@ -49,6 +67,22 @@ excel_data = pd.concat([pd.DataFrame(x_test), test], axis=1)
 
 # df = pd.DataFrame(excel_data)
 
-# excel_data.to_excel("result.xlsx", index=False)
+excel_data.to_excel("result.xlsx", index=False)
 
-print('accuracy is',accuracy_score(y_pred,y_test) * 100) # menghitung akurasi
+print('accuracy is',accuracy_score(y_pred,y_test) * 100) # menghitung 
+
+correct_predictions = (y_pred == y_test).sum()
+incorrect_predictions = len(y_test) - correct_predictions
+
+print('Prediksi yang benar:', correct_predictions)
+print('Prediksi yang salah:', incorrect_predictions)
+
+# detailkan yang benar dan yang salah dari data uji
+print()
+print('Detail prediksi yang benar:')
+true_pred = x_test[y_pred == y_test]
+print(x_test[y_pred == y_test])
+print()
+print('Detail prediksi yang salah:')
+print(x_test[y_pred != y_test])
+print()
